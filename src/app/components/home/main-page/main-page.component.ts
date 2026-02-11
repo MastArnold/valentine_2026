3196d86cd55c53dabfb78d58c99eae0c8f76b668
Lossi Arnold Jonathan <102259623+MastArnold@users.noreply.github.com>
2026-02-11 10:17:36 +0000

    add: cv section

--- src/app/components/home/main-page/main-page.component.ts
+++ src/app/components/home/main-page/main-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavComponent } from "../nav/nav.component";
 import { HeaderComponent } from "../header/header.component";
+import { CvSectionComponent } from "../cv-section/cv-section.component";
 
 @Component({
   selector: 'app-main-page',
-  imports: [NavComponent, HeaderComponent],
+  imports: [NavComponent, HeaderComponent, CvSectionComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
