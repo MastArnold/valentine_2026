2b78b38c9a51ca1152ee4185106327b029eda646
Lossi Arnold Jonathan <102259623+MastArnold@users.noreply.github.com>
2026-02-11 09:41:52 +0000

    feature: music main lyrics

--- src/app/components/home/header/header.component.ts
+++ src/app/components/home/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { Lyric } from '../../../interfaces/lyric.interface';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +7,73 @@ import { Component, signal } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
-  lyrics = signal(false);
+export class HeaderComponent implements OnInit{
+  lyricOverlay = signal(false);
+  lyrics : Lyric[] = [];
+
+  ngOnInit(): void {
+    this.initLyrics();
+  }
+
+  initLyrics(){
+    this.lyrics = [
+      {
+        eng: "I see the crytal raindrops fall",
+        fr: "Je vois les crytaux de pluies tomberrrr"
+      },
+      {
+        eng: "And the beauty of it all",
+        fr: "Et de toute sa beauté"
+      },
+      {
+        eng: "When the sun comes shining through",
+        fr: "Quand le soleil passe à travers"
+      },
+      {
+        eng: "To make those rainbows in my minds",
+        fr: "Pour faire ces arc-en-ciel dans ma tête"
+      },
+      {
+        eng: "When i think of you sometimes",
+        fr: "Quand je pense à toi souvent"
+      },
+      {
+        eng: "And i wanna spend some times with you",
+        fr: "Et je veux passer du temps avec toi"
+      },
+      {
+        eng: "Juuuust the two of us (ahhhhhhhhhhh)",
+        fr: "Juste tous les deux (mdr c'est trop nul en français)"
+      },
+      {
+        eng: "We can make it we try",
+        fr: "On peut le faire si on essaie"
+      },
+      {
+        eng: "Just the two of us",
+        fr: "Juste tous les deux"
+      },
+      {
+        eng: "(Just the two of us)",
+        fr: "(Juste tous les deux)"
+      },
+      {
+        eng: "Juuuust the two of us",
+        fr: "Juste tous les deux"
+      },
+      {
+        eng: "Building castle in the sky",
+        fr: "Construire un chateau dans le ciel"
+      },
+      {
+        eng: "Just the two of us",
+        fr: "Juste tous les deux"
+      },
+      {
+        eng: "You and I ",
+        fr: "Toi et moi"
+      }
+    ];
+  }
+
 }
