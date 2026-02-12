202f9fc4ee208c6c2c4eaa691af3300e087d58f4
Lossi Arnold Jonathan <102259623+MastArnold@users.noreply.github.com>
2026-02-12 22:48:09 +0000

    refactor: disquette section

--- src/app/components/home/main-page/main-page.component.ts
+++ src/app/components/home/main-page/main-page.component.ts
@@ -3,10 +3,11 @@ import { NavComponent } from "../nav/nav.component";
 import { HeaderComponent } from "../header/header.component";
 import { CvSectionComponent } from "../cv-section/cv-section.component";
 import { ProgramComponent } from "../program/program.component";
+import { DisquetteSectionComponent } from "../disquette-section/disquette-section.component";
 
 @Component({
   selector: 'app-main-page',
-  imports: [NavComponent, HeaderComponent, CvSectionComponent, ProgramComponent],
+  imports: [NavComponent, HeaderComponent, CvSectionComponent, ProgramComponent, DisquetteSectionComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
