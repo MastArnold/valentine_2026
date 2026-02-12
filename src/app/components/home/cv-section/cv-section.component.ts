23fd598fe10a26d297886067760bedcd5f71de8d
Lossi Arnold Jonathan <102259623+MastArnold@users.noreply.github.com>
2026-02-12 10:47:36 +0000

    refactor: cv section

--- src/app/components/home/cv-section/cv-section.component.ts
+++ src/app/components/home/cv-section/cv-section.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { NgClass, NgStyle } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cv-section',
-  imports: [],
+  imports: [NgClass, NgStyle],
   templateUrl: './cv-section.component.html',
   styleUrl: './cv-section.component.scss'
 })
-export class CvSectionComponent {
+export class CvSectionComponent implements OnInit{
+  photoTranslatePosition = 0;
+  photoTranslateCalc = '';
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.translateTimer();
+    }, 3000);
+  }
+
+  translateTimer(){
+    this.photoTranslatePosition += 100;
+    this.photoTranslateCalc = '-'+this.photoTranslatePosition+'% - '+(this.photoTranslatePosition/100)+'rem';
+    
+    if(this.photoTranslatePosition > 500){
+      this.photoTranslatePosition = 0;
+      this.photoTranslateCalc = '-'+this.photoTranslatePosition+'%';
+    }
+    
+    setTimeout(() => {
+      this.translateTimer();
+    }, 3600);
+  }
 
 }
